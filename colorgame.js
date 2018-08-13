017f9626c63391fe3e837840b1be578dd89b36a4
Fiona Gridley <gridley.f@husky.neu.edu>
2018-08-13 19:19:34 -0400

    cleaning up colorgame.js

--- colorgame.js
+++ colorgame.js
@@ -8,15 +8,42 @@ var loader2 = loader.querySelector(".loader2");
 var loader3 = loader.querySelector(".loader3");
 var targetDisplay = document.querySelector("#targetDisplay");
 var endDisplay = document.querySelector(".endDisplay");
-var gameWonBool = false;
 var tryAgain = document.querySelector(".restartBtn");
+var gameWonBool = false;
 
-loadAnimation();
-circle.addEventListener("click", function() {
-  restart();
-})
-tryAgain.addEventListener("click", restart);
+onLoad();
 
+// set colors and event listeners when game is initiated
+function onLoad() {
+  loadAnimation();
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click", function() {
+      if (this.style.backgroundColor === target && !gameWonBool) {
+        gameWon();
+        gameWonBool = true;
+      } else if (!gameWonBool) {
+        this.style.backgroundColor = "#434343"
+      }
+    })
+  }
+  targetDisplay.innerHTML = target;
+  tryAgain.addEventListener("click", restart);
+  circle.addEventListener("click", function() {
+    restart();
+  })
+}
+
+// show loading animation when new game starts
+function loadAnimation() {
+  loader.classList.add("loadergo");
+  loader1.classList.add("loader1go");
+  loader2.classList.add("loader2go");
+  loader3.classList.add("loader3go");
+  setTimeout(removeLoad, 1600);
+}
+
+// reset colors and add/remove necessasry css classes when game restarts
 function restart() {
   colors = randomColors(6);
   target = colors[Math.floor(Math.random() * colors.length)];
@@ -28,17 +55,9 @@ function restart() {
   circle.classList.remove("circlegrow");
   endDisplay.classList.remove("showEndDisplay");
   gameWonBool = false;
-
 }
 
-function loadAnimation() {
-  loader.classList.add("loadergo");
-  loader1.classList.add("loader1go");
-  loader2.classList.add("loader2go");
-  loader3.classList.add("loader3go");
-  setTimeout(removeLoad, 1600);
-}
-
+//remove load animation class from elements when animation is over
 function removeLoad() {
   loader.classList.remove("loadergo");
   loader1.classList.remove("loader1go");
@@ -46,18 +65,12 @@ function removeLoad() {
   loader3.classList.remove("loader3go");
 }
 
-for (var i = 0; i < squares.length; i++) {
-  squares[i].style.backgroundColor = colors[i];
-  squares[i].addEventListener("click", function() {
-    if (this.style.backgroundColor === target && !gameWonBool) {
-      gameWon();
-      gameWonBool = true;
-    } else if (!gameWonBool) {
-      this.style.backgroundColor = "#434343"
-    }
-  })
+// when the correct color is guessed, show winning animation/display
+function gameWon() {
+  circle.style.background = target;
+  circle.classList.add("circlegrow");
+  endDisplay.classList.add("showEndDisplay");
 }
-targetDisplay.innerHTML = target;
 
 
 function randomColors(numColors) {
@@ -73,15 +86,4 @@ function randomColor() {
   var green = Math.floor(Math.random() * 256);
   var blue = Math.floor(Math.random() * 256);
   return "rgb(" + red + ", " + green + ", " + blue + ")";
-}
-
-function gameWon() {
-  // document.body.style.backgroundColor = target;
-  // for (var i = 0; i < squares.length; i++) {
-  //   squares[i].style.backgroundColor = target;
-  // }
-  circle.style.background = target;
-  circle.classList.add("circlegrow");
-  endDisplay.classList.add("showEndDisplay");
-
 }
